ee9e53b0c32b811f9d231ca4bda52bdeac07b43c
HairyPotato69 <jimmyding9719@gmail.com>
2024-04-19 11:56:53 +0800

    moved files
    dumb utar gave me worm

--- database final pract/P10/CreateTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE my_employee
-(id  NUMBER(4) CONSTRAINT my_employee_id_pk primary key,
- last_name VARCHAR2(25) NOT NULL,
- first_name VARCHAR2(25),
- userid  VARCHAR2(8),
- salary  NUMBER(9,2));
